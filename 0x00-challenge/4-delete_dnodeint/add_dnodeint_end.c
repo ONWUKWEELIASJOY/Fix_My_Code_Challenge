f67b6f85cca71696830b382579602deff154a193
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2023-05-28 10:54:33 -0700

    debug

---
+++ 0x00-challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Add a node at the end of a list
+ *
+ * @head: The address of the pointer to the first element of the list
+ * @n: The number to store in the new element
+ *
+ * Return: A pointer to the new element
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *a;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		new->prev = NULL;
+		return (new);
+	}
+	a = *head;
+	while (a->next != NULL)
+	{
+		a = a->next;
+	}
+	a->next = new;
+	new->prev = a;
+	return (new);
+}
---
+++ 0x00-challenge/4-delete_dnodeint/free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Free a list
+ *
+ * @head: A pointer to the first element of the list
+ *
+ * holbertonschool
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
